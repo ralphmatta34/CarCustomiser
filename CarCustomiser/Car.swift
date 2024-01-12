8479ef2a886d9d830fe6c781d83f2f818d69f147
ralphmatta3 <ralphmatta3@gmail.com>
2024-01-12 13:13:39 +0000

    Implemented TDD unit tests for Car struct and displayStats method

--- CarCustomiser/Car.swift
+++ CarCustomiser/Car.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+    
+    func displayStats() -> String {
+        
+        let string = """
+                        Make: \(self.make)\n
+                        Model: \(self.model)\n
+                        Top Speed: \(self.topSpeed)mph \n
+                        Acceleration (0-60): \(self.acceleration)s \n
+                        Handling: \(self.handling)
+                        """
+        
+        return string
+        
+    }
+}
--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,13 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        Text(car.displayStats())
         .padding()
     }
 }

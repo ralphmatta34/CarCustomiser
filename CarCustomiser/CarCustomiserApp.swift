dd290ee93bfc13ca53ed2e4d79129e0872121b2d
ralphmatta3 <ralphmatta3@gmail.com>
2024-01-12 10:53:19 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Ralph Matta on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarCustomiser
+//
+//  Created by Ralph Matta on 12/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

99ae69eadb7d186a97868ace5bbf8fc992511342
ralphmatta34 <matta.r@etoncollege.org.uk>
2024-01-31 23:56:32 +0000

    Finished Racing Car Customiser 2: encountered some unresolved logic errors with the toggles and remaining funds.

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar: Int = 0
+    
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    
+    @State private var remainingFunds: Int = 750
+    
+    @State private var justChanged: Bool = false
     
     var body: some View {
-        Text(car.displayStats())
-        .padding()
+        let exhaustPackageBinding = Binding<Bool> (
+            get: { self.exhaustPackage },
+            set: { newValue in
+                self.exhaustPackage = newValue
+                if newValue && self.remainingFunds >= 500 {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                    self.remainingFunds -= 500
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                    self.remainingFunds += 500
+                }
+            }
+        )
+        
+        let tiresPackageBinding = Binding<Bool> (
+            get: { self.tiresPackage },
+            set: { newValue in
+                self.tiresPackage = newValue
+                if newValue && self.remainingFunds >= 500 {
+                    starterCars.cars[selectedCar].handling += 2
+                    self.remainingFunds -= 500
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                    self.remainingFunds += 500
+                }
+            }
+        )
+    
+        VStack {
+            Form {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(starterCars.cars[selectedCar].displayStats())
+                    
+                    Button("Next Car", action: {
+                        
+                        exhaustPackage = false
+                        tiresPackage = false
+                        justChanged = true
+                        
+                        if selectedCar == starterCars.cars.count-1 {
+                            selectedCar = 0
+                            
+                        } else {
+                            selectedCar += 1
+                        }
+                        
+                        if exhaustPackage || tiresPackage {
+                            justChanged = false
+                        }
+                        
+                    })
+                }
+                    
+                Section {
+                        
+                    Toggle("Exhaust Package (500)", isOn: exhaustPackageBinding)
+                        .disabled(self.remainingFunds < 500 && !exhaustPackage && !justChanged)
+                        
+                    Toggle("Tires Package (500)", isOn: tiresPackageBinding)
+                        .disabled(self.remainingFunds < 500 && !tiresPackage && !justChanged)
+         
+                }
+                
+            }
+            
+            Text("Remaining Funds: \(self.remainingFunds)")
+                .foregroundColor(.red)
+                .baselineOffset(20)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,15 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Ralph Matta on 29/01/2024.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
+                Car(make: "Ford", model: "GX-2", topSpeed: 115, acceleration: 7.4, handling: 4),
+                Car(make: "BMW", model: "Sport", topSpeed: 140, acceleration: 8.3, handling: 6),
+                Car(make: "Toyota", model: "E", topSpeed: 105, acceleration: 6.7, handling: 3)]
+}
